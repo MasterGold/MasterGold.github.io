3ea6e9ef00db48dca147ae9bb773eb80db8e8e5d
MasterGold <ryankurtis1@gmail.com>
2016-05-21 16:41:09 +0100

    Update peter.php

--- pages/peter.php
+++ pages/peter.php
@@ -1,1 +1,38 @@
 
+<html>
+	<head>
+		<title>Peter</title>
+		<link rel="stylesheet" type="text/css" href="/css/main.css">
+		<link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
+	</head>
+	
+	<body>
+	  <footer class="top">
+	    <ul>
+	      <li><a href="/index.html">Banana</a></li>
+	      <li><a href="/pages/second.html">Cantaloupe</a></li>
+	      <li><a href="/pages/conor.html">Conor</a></li>
+	      <li><a href="/pages/peter.html">Peter.html</a></li>
+	    </ul>
+	  </footer>
+	  <div class="container">
+    		<div class="blurb">
+        		<h1>Peter</h1>
+			<p>Here we see the wild Peter:</p>
+			<div>
+				<img height="720" src="https://7213596f-a-62cb3a1a-s-sites.googlegroups.com/site/medicologyofficial/home/peter%20mlg.png?attachauth=ANoY7cp4mhnw8XKB-zyJfd0a940R16aSvqURlw-8F6C8AgHNYBEyORe1qM2s8eSfdIzGHFUvC7Dh5MHoEM1cfQ4iP7mhjUELqOIC6ZJR6CCJr3lTUl658tCs8WUOImlAJp71zWSP7XGjWcxPT3uMlF4grw-6umJEmfurTlVkYqGq0UglL3gkPOdwbl5zTPzyY69DXSITm4Gg3c2HBSN1WyFVdgaT3guTqWcd7uyJKhIbBiXzlrAwnc0%3D&attredirects=0" width="1280" title="MLG Peter James Atkinson Sir">		
+				</div>
+				<div>
+				  <?php 
+				    $myName = "Ryan";
+				    echo $myName;
+				  
+				  
+				  ?>
+				</div>
+		
+    		</div><!-- /.blurb -->
+	</div><!-- /.container -->
+	</body>
+</html>
+
